a5649807ce3ac3d772654322e57eaaa7d440515c
Raufull Islam Rauf <152892595+Rauf-17@users.noreply.github.com>
2024-02-17 11:26:46 +0600

    Add files via upload

---
+++ Teacher/src/Teacher.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class Teacher {
+
+	String Name, Department, Gender;
+	int Age;
+	long Phone;
+}
---
+++ Teacher/src/Test.java
@@ -0,0 +1,23 @@
+
+public class Test {
+
+	public static void main (String[]args) {
+		
+		Teacher teacher1 = new Teacher();
+		
+		teacher1.Name = "Mahmud Shahi";
+		teacher1.Department = "CSE";
+		teacher1.Gender = "Male";
+		teacher1.Age= 34;
+		teacher1.Phone = 364634;
+		
+		System.out.println("Name = " + teacher1.Name);
+		System.out.println("Department = " + teacher1.Department);
+		System.out.println("Gender = " + teacher1.Gender);
+		System.out.println("Age = " + teacher1.Age);
+		System.out.println("Phone = " + teacher1.Phone);
+		
+	    //System.out.println("I love you too jaanu");
+	
+	}
+}
